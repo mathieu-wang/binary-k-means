b2e115f5084e5fee72d7b8a63049f26a4aff7030
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-11-25 22:08:47 -0500

    Load data from .mat file and get binary str representation

---
+++ multi_index_hashing.py
@@ -0,0 +1,11 @@
+import sys
+import numpy as np, h5py
+
+f = h5py.File('lsh_64_sift_1M.mat', 'r')
+data = f.get('B')
+data = np.array(data) # For converting to numpy array
+print data[0]
+for i in data[0]:
+    b = bin(i)[2:].zfill(8)
+    print sys.getsizeof(b)
+
