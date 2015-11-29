b2f6aac7b025bf8c2be2846ad93c730ea867f139
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-11-28 23:27:08 -0500

    Add working hamming distance function

--- kmeans.py
+++ kmeans.py
@@ -1,7 +1,16 @@
 import random
 
-
 MAX_ITERATIONS = 100
+
+
+def hamming_dist(num1, num2):
+    dist = 0
+    for i in xrange(len(num1)):
+        diff = num1[i] ^ num2[i]
+        cur_dist = bin(diff).count("1")
+        dist += cur_dist
+    return dist
+
 
 # _function: _should _stop
 # -------------
@@ -12,31 +21,32 @@ def should_stop(old_centroids, centroids, iterations):
     if iterations > MAX_ITERATIONS: return True
     return old_centroids == centroids
 
+
 # _function: _get _labels
 # -------------
 # _returns a label for each piece of data in the dataset.
-def get_labels(data_set, centroids):
+def get_labels(dataset, centroids):
     # _for each element in the dataset, chose the closest centroid.
     # _make that centroid the element's label.
+    # for datum in dataset:
     pass
+
 
 # _function: _get _centroids
 # -------------
 # _returns k random centroids, each of dimension n.
-def get_centroids(data_set, labels, k):
+def get_centroids(dataset, labels, k):
     # _each centroid is the geometric mean of the points that
     # have that centroid's label. _important: _if a centroid is empty (no points have
     # that centroid's label) you should randomly re-initialize it.
     pass
 
 
+# Returns k centroids chosen randomly
 def get_random_centroids(dataset, k):
     length = len(dataset)
-    return [random.randint(0, length) for _ in xrange(k)]
+    return [dataset[random.randint(0, length)] for _ in xrange(k)]
 
-
-def get_num_features(dataset):
-    pass
 
 # _function: _k _means
 # -------------
@@ -46,23 +56,21 @@ def get_num_features(dataset):
 def kmeans(dataset, k):
 
     # _initialize centroids randomly
-    num_features = get_num_features(dataset)
-    centroids = get_random_centroids(num_features, k)
+    # num_features = get_num_features(dataset)
+    centroids = get_random_centroids(dataset, k)
 
-    # _initialize book keeping vars.
     iterations = 0
     old_centroids = None
 
     # _run the main k-means algorithm
     while not should_stop(old_centroids, centroids, iterations):
-        # _save old centroids for convergence test. _book keeping.
         old_centroids = centroids
         iterations += 1
 
-        # _assign labels to each datapoint based on centroids
+        # Assign labels to each datapoint based on centroids
         labels = get_labels(dataset, centroids)
 
-        # _assign centroids based on datapoint labels
+        # Assign centroids based on datapoint labels
         centroids = get_centroids(dataset, labels, k)
 
     # _we can get the labels too by calling get_labels(data_set, centroids)
--- multi_index_hashing.py
+++ multi_index_hashing.py
@@ -8,6 +8,15 @@ data = np.array(data) # For converting to numpy array
 # print data[0]
 # for i in data[0]:
 #     b = bin(i)[2:].zfill(8)
-#     print sys.getsizeof(b)
+# #     print sys.getsizeof(b)
+#     print b,
+#
+# print ""
+# for i in data[1]:
+#     b = bin(i)[2:].zfill(8)
+#     print b,
 
-print kmeans.get_random_centroids(data, 10)
+# print kmeans.get_random_centroids(data, 10)
+print kmeans.hamming_dist(data[0], data[1])
+# print kmeans.hamming_dist([7, 6], [4, 5])
+# array([181,  53, 218,  18, 218, 169, 186, 125], dtype=uint8), array([ 61,  27, 146,  50, 138, 193, 167, 231], dtype=uint8)
