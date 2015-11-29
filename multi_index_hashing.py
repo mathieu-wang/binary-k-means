b51d52e17dcfdea2876ecedd91c34c88f616fcc5
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-11-29 00:08:52 -0500

    Add timer

--- multi_index_hashing.py
+++ multi_index_hashing.py
@@ -1,6 +1,6 @@
-import sys
 import numpy as np, h5py
 import kmeans
+import time
 
 f = h5py.File('lsh_64_sift_1M.mat', 'r')
 data = f.get('B')
@@ -19,5 +19,8 @@ data = np.array(data) # For converting to numpy array
 # print kmeans.get_random_centroids(data, 10)
 # print kmeans.hamming_dist(data[0], data[1])
 # print kmeans.hamming_dist([7, 6], [4, 5])
-print kmeans.find_closest_centroid(data[0], kmeans.get_random_centroids(data, 100))
+
+start_time = time.time()
+print kmeans.find_closest_centroid(data[0], kmeans.get_random_centroids(data, 100000))
+print("--- %s seconds ---" % (time.time() - start_time))
 # array([181,  53, 218,  18, 218, 169, 186, 125], dtype=uint8), array([ 61,  27, 146,  50, 138, 193, 167, 231], dtype=uint8)
