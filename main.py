56d6b33dc292469c108a09ef4fd2bd9e821d525e
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-12-01 14:57:33 -0500

    Fix add to MIH table

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np, h5py
 import time
 import utils
 from kmeans import kmeans
+from multi_index_hashing import MihTable
 import sys
+from pprint import pprint
 
 f = h5py.File('lsh_64_sift_1M.mat', 'r')
 data = f.get('B')
@@ -10,8 +12,14 @@ data = np.array(data)  # For converting to numpy array
 data = data[0:1000]
 
 start_time = time.time()
-centroids = kmeans(data, 2, 64)
-print centroids
+# centroids = kmeans(data, 2, 64)
+# print centroids
+
+mih_table = MihTable(64, 64/8, 10)
+for bit_vector in data:
+    mih_table.add(bit_vector)
+pprint(mih_table.hash_tables)
+
 print("--- %s seconds ---" % (time.time() - start_time))
 
 # array([181,  53, 218,  18, 218, 169, 186, 125], dtype=uint8), array([ 61,  27, 146,  50, 138, 193, 167, 231], dtype=uint8)
--- multi_index_hashing.py
+++ multi_index_hashing.py
@@ -5,13 +5,19 @@ def split_chunks(bit_vector, M):
     chunks = bit_vector
     return chunks
 
-def generate_close_chunks(chunks, K):
+
+def generate_close_chunks(chunk, K):
     # returns a list of all possible chunks that is zero or one bit off of the input
     # I'll also leave this one for the reader
     # eg: find_close_chunks(0000) -> [0000, 1000, 0100, 0010, 0001]
     # eg: find_close_chunks(1011) -> [1011, 0011, 0111, 1001, 1010]
     close_chunks = []
+    close_chunks.append(chunk)
+
+    # TODO generate codes at specific hamming distance
+
     return close_chunks
+
 
 class MihTable:
     def __init__(self, num_bits, num_chunks, radius):
@@ -20,7 +26,7 @@ class MihTable:
         self.M = num_chunks
         self.R = radius
 
-        self.hash_tables = []  # a list of M hash tables
+        self.hash_tables = [dict() for _ in xrange(self.M)]  # a list of M hash tables
 
     def add(self, bit_vector):
         chunks = split_chunks(bit_vector, self.M)
@@ -30,11 +36,9 @@ class MihTable:
             chunk = chunks[i]  # fetch the ith chunk of the bit_vector
 
             if chunk not in hash_table:
-                hash_table[chunk] = []
+                hash_table[chunk] = []  # make empty bit vector list
 
-            self.bit_vector_list = hash_table[chunk]
-            self.bit_vector_list.append(bit_vector)
-
+            hash_table[chunk].append(bit_vector)
 
     # K-NN search
     def lookup(self, bit_vector, K):
@@ -46,14 +50,14 @@ class MihTable:
             hash_table = self.hash_tables[i]
             chunk = chunks[i]
 
-            possible_matches = []
+            matches = []
             close_chunks = generate_close_chunks(chunk, K)
 
             for close_chunk in close_chunks:
                 if close_chunk in hash_table:
-                    possible_matches.append(hash_table[close_chunk])
+                    matches.append(hash_table[close_chunk])
 
-            if len(possible_matches) > 0:
-                return possible_matches
+            if len(matches) > 0:
+                return matches
 
         return []
